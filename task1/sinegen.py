fa2c4ff41449ac966b0d55ab47a1c83f3a10e198
Abeeekoala <abraham@LAPTOP-PBSLHUIS>
2024-10-30 00:20:08 +0000

    Lab 2 task 1 + challenge done

--- task1/sinegen.py
+++ task1/sinegen.py
@@ -1,8 +1,9 @@
 import math
 import string
 f = open("sinerom.mem","w")
-for i in range(256):
-    v = int(math.cos(2*3.1416*i/256)*127+127)
+for i in range(256): # 256 steps
+    v = int(math.cos(2*3.1416*i/256)*127+127) # 256 is 1 (max, high), 127 is 0, 0 is -1 (min, low) 
+    # Format it such that 16 numbers a row
     if (i+1)%16 == 0:
         s = "{hex:2X}\n"
     else:
---
+++ task1/sinegen_tb.cpp
@@ -0,0 +1,49 @@
+#include "Vsinegen.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "vbuddy.cpp"
+
+int main(int argc, char **argv, char **env){
+    int i;
+    int clk;
+
+    Verilated::commandArgs(argc, argv);
+    // init top verilog instance
+    Vsinegen* top = new Vsinegen;
+
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+
+    top->trace(tfp, 99);
+    tfp->open ("sinegen.vcd");
+
+    //init Vbuddy
+    if (vbdOpen() != 1) return(-1);
+    vbdHeader("Lab 2: Sinegen");
+    vbdSetMode(1);
+
+    top->clk = 1;
+    top->rst = 0;
+    top->en = 1;
+    top->incr = 1;
+    //run simulation for many clock cycles
+    for (i=0; i<1000000; i++){
+        top->incr = vbdValue();
+        for (clk=0; clk<2; clk++){
+            tfp->dump (2*i+clk);
+            top->clk = !top->clk;   // unit is in ps!!!
+            top->eval ();
+        }
+
+        vbdPlot(int(top->dout), 0, 255);
+        
+        vbdCycle(i+1);
+            // either simulation finished, or 'q' is pressed
+        if ((Verilated::gotFinish()) || (vbdGetkey()=='q')) 
+            exit(0);                // ... exit if finish OR 'q' pressed
+        if (Verilated::gotFinish()) exit(0);
+
+    }
+    tfp->close();
+    exit(0);
+}
